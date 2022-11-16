0436fa5e8ed986cc1cf0cfb0888d281d204653b1
perinazzoo <gabrielperinazo@gmail.com>
2022-11-16 01:54:13 -0300

    chore: title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@material-tailwind/react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+      <title>Formatura Nath - Lista</title>
+      </Head>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   )
 }
